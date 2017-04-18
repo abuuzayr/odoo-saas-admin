b8df4f29f89989ff40095b7fa54269bc2fc2ef8b
Hellyna NG <hellyna@groventure.com>
2017-04-18 12:00:34 +0800

    Initial commit.

---
+++ smecen_saas_admin/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'SMECEN SAAS Admin',
+    'version' : '1.1',
+    'summary': 'Manages administration of SMECEN Infrastructure',
+    'sequence': 30,
+    'description': 'Manages administration of SMECEN Infrastructure',
+    'depends' : [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'Other Proprietary',
+}
