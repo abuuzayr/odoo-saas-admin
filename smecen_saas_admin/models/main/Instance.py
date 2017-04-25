f9bafc91a3dd5ec396e766ce305c97438c5d48de
Hellyna NG <hellyna@groventure.com>
2017-04-25 17:17:12 +0800

    Use process call instead.

--- smecen_saas_admin/models/main/Instance.py
+++ smecen_saas_admin/models/main/Instance.py
@@ -1,4 +1,4 @@
-from openerp import fields, models
+from openerp import api, fields, models
 from openerp.addons.smecen_saas_admin.lib.ansible import Playbook
 
 class Instance(models.Model):
@@ -8,19 +8,20 @@ class Instance(models.Model):
     name = fields.Char()
     host_id = fields.Many2one('smecen_admin.host', string='Host')
 
+
+    @api.multi
     def start_instance(self):
         pb = Playbook(
             playbooks=['hello.yml'],
             options={
-                'remote_user': self.host_id.user,
+                'user': self.host_id.user,
                 'inventory': '{0},'.format(self.host_id.ip),
-                'verbosity': 2,
-                'extra_vars': [
+                'extra-vars': ','.join([
                     'hello_name=hellyna',
-                    'ansible_port={0}'.format(self.host_id.port),
-                ],
+                ]),
                 # TODO: Secure host key checking
-                'ssh_common_args': '-o StrictHostKeyChecking=no'
+                'ssh-common-args': '-o StrictHostKeyChecking=no -p {0}'.format(
+                    self.host_id.port),
             },
         )
         pb.play()
